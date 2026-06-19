5748ca97762f46d79c6f2d78f509a40f774d879b
srujansai122 <saisrujangundeti@gmail.com>
2026-06-19 13:41:24 +0530

    Implemented Product Detail Module

--- FoodSiteWithOrderManagement/myfoodshop/foodshop/views.py
+++ FoodSiteWithOrderManagement/myfoodshop/foodshop/views.py
@@ -6,4 +6,9 @@ from .models import Product
 def index(request):
     products=Product.objects.all();
     return render(request,'foodshop/index.html',{'products':products})
-    
+
+
+
+def detail(request,slug):
+    product=Product.objects.get(slug=slug)
+    return render(request,"foodshop/detail.html",{'product':product})
